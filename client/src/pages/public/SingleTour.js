f9f6bf452b2b0ba6cc6ec3e2772eae2294eb5a80
cherin piyumantha <cherinpiyumantha@gmail.com>
2023-02-06 12:59:02 +0530

    single tour map component commented

--- client/src/pages/public/SingleTour.js
+++ client/src/pages/public/SingleTour.js
@@ -10,7 +10,7 @@ const SingleTour = () => {
             <SigleTourHeader />
             <SIngleTourOverview />
             <SingleTourIncludes />
-            <SingleTourMap/>
+            {/* <SingleTourMap/> */}
         </>
     );
 }
