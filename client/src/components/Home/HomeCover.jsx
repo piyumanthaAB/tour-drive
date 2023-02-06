6a6d690fcf5fbe9fe6b9353d39129c9016393947
vmsoory <vmsoory@gmail.com>
2023-02-06 08:10:26 +0530

    Homcover edit

--- client/src/components/Home/HomeCover.jsx
+++ client/src/components/Home/HomeCover.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import * as h from './HomeCoverElements';
-
+import React from "react";
+import * as h from "./HomeCoverElements";
 const HomeCover = () => {
   return (
-      <>
-          <h.CoverContainer></h.CoverContainer>
-    </>
-  )
-}
+    <>
+      <h.section>
+        <h.panel>
+          <div className="overlay"></div>
+          <h.tourH1>
+            <h1>TOUR</h1>
+          </h.tourH1>
+          <h.btnTour>
+            <a href="tours" className="btn">
+              GET STARTED
+            </a>
+          </h.btnTour>
 
-export default HomeCover
+          <div className="test">tour search</div>
+        </h.panel>
+        <h.panel>
+          <h.driveH1>
+            <h1>DRIVE</h1>
+          </h.driveH1>
+          <h.btnDrive>
+            <a href="#" className="btn">
+              GET STARTED
+            </a>
+          </h.btnDrive>
+          <div className="test">renting search</div>
+        </h.panel>
+      </h.section>
+    </>
+  );
+};
+
+export default HomeCover;
--- client/src/components/Home/HomeCoverElements.js
+++ client/src/components/Home/HomeCoverElements.js
@@ -1,14 +1,120 @@
 import styled from "styled-components";
-import cover from './../../images/home_cover.jpg'
+import tour from "../../images/tour_back.jpg";
+import renting from "../../images/renting.jpg";
 
-export const CoverContainer = styled.div`
 
-    width:100% ;
-    min-height:100vh ;
-    background-image: url(${cover});
-    background-position: center;
-    background-repeat: no-repeat;
-    background-size: cover;
+export const section = styled.div`
+    min-width: 100vw;
+    width: 100%;
+    height: 800px;
+    display: flex;
+    flex-direction: row;
 
+    
+.overlay{
+    background-color: black;
+    position: absolute;
+    opacity: 0.3;
+    width: 100%;
+    height: 100%;
+    transition: all 1s ease;
+}
 `;
+
+
+
+const hover = (`
+   
+    .test {
+            display: none;
+        }
+        &:hover {
+            flex-grow: 3;
+            .test {
+                display: block;
+            }
+        }
+`)
+
+export const panel = styled.div`
+    padding: 0px;
+    flex-grow: 1;
+    transition: all 1s ease;
+    &:hover .overlay{
+        opacity: 1;
+    }
+    &:nth-child(even) {
+        background-image: url(${renting});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        ${hover}
+    }
+    &:nth-child(odd) {
+        
+        background-image: url(${tour});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+
+        ${hover}
+    }
+`;
+export const btnDrive = styled.a`
+    display: flex;
+    flex-direction: row;
+    padding: 70px;
+    z-index: 9999;
+    .btn {
+        padding: 1.5rem;
+        width: 16rem;
+        text-decoration: none;
+        border: 3px solid #fff;
+        color: #fff;
+        font-weight: bold;
+        text-transform: uppercase;
+        text-align: center;
+        box-shadow: 0px 10px 10px -5px rgba(0,0,0,0.1)
+    }
+`;
+export const btnTour = styled.a`
+    display: flex;
+    flex-direction: row-reverse;
+    padding: 70px;
+    z-index: 9998;
+    .btn {
+        padding: 1.5rem;
+        width: 16rem;
+        text-decoration: none;
+        border: 3px solid #fff;
+        color: #fff;
+        font-weight: bold;
+        text-transform: uppercase;
+        text-align: center;
+        box-shadow: 0px 10px 10px -5px rgba(0,0,0,0.1)
+    }
+`;
+ export const tourH1 = styled.div`
+    padding-right: 40px;
+    z-index: 9997;
+    filter: blur(0px);
+    display: flex;
+    flex-direction: row-reverse;
+    font-size: 80px;
+    font-family: var(--secondary-font);
+    font-weight: bold;
+    color: var(--secondary-color)
+    }
+ `;
+ export const driveH1 = styled.div`
+    padding-left: 40px;
+    z-index: 9996;
+    display: flex;
+    flex-direction: row;
+    font-size: 80px;
+    font-family: var(--secondary-font);
+    font-weight: bold;
+    color: var(--main-color);
+ `;
 
