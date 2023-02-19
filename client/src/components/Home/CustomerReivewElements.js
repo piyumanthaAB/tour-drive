0298a6aef7966078fc76c716d91f90682e464af2
vmsoory <vmsoory@gmail.com>
2023-02-19 11:57:52 +0530

    clean dev branch by removing CusReview

--- client/src/components/Home/CustomerReivewElements.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const ReviewSection = styled.div`
-    width: 100%;
-    min-height: 20rem;
-    padding: 5rem 5rem;
-    background-color: blue;
-`;
