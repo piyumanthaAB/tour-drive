5ee51e284322c6310112b24d7609183f50a6cbeb
vmsoory <vmsoory@gmail.com>
2023-03-21 12:13:09 +0530

    home cover size fixed

--- client/src/pages/public/Home.js
+++ client/src/pages/public/Home.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import HomeCover from '../../components/Home/HomeCover'
-import HomeCoverNew from '../../components/Home/HomeCoverNew'
-import TourSlider from '../../components/Home/TourSlider'
-import CustomerReview from '../../components/Home/CustomerReview'
-import VehicleSlider from '../../components/Home/VehicleSlider'
-
+import React from "react";
+import HomeCover from "../../components/Home/HomeCover";
+import HomeCoverNew from "../../components/Home/HomeCoverNew";
+import TourSlider from "../../components/Home/TourSlider";
+import CustomerReview from "../../components/Home/CustomerReview";
+import VehicleSlider from "../../components/Home/VehicleSlider";
 
 const Home = () => {
   return (
     <>
-      <HomeCover/>
+      <HomeCover />
       {/* <HomeCoverNew/> */}
-      <TourSlider/>
-      <VehicleSlider/>
-      <CustomerReview/>
+      <TourSlider />
+      <VehicleSlider />
+      <CustomerReview />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
