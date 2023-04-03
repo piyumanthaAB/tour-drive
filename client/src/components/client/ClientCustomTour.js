6a8a958c71b2f5e81b8749474bf684f7117902a4
vmsoory <vmsoory@gmail.com>
2023-04-03 18:24:18 +0530

    create client custom tour view

--- client/src/components/client/ClientCustomTour.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ClientCustomTour = () => {
-  return <h3>this is the custom tour create component.</h3>;
-};
-
-export default ClientCustomTour;
---
+++ client/src/components/client/ClientCustomTour.jsx
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import * as c from "./ClientCustomTourElements";
+import Label from "../../components/shared/Form Elements/Label";
+import TextField from "../../components/shared/Form Elements/TextField";
+import DropDown from "../../components/shared/Form Elements/DropDown";
+import TextArea from "../shared/Form Elements/TextArea";
+
+const ClientCustomTour = () => {
+  const availableCategory = [
+    "hiking",
+    "camping",
+    "Pilgrimage",
+    "traditional excursion",
+  ];
+
+  const availableVehicle = [
+    "Car (economy 4 Passengers 2 bags)",
+    "car (standard 5 Passengers 3 bags)",
+    "car (Premium 5 passengers 4 bags)",
+    "SUV (Compact 5 passengers 3 bags)",
+    "SUV (Standard 5 passengers 4 bags)",
+    "SUV (Premium 6 passengers 5 bags)",
+    "Minivan (7-8 passengers 5 bags)",
+    "van (8-12 passengers 8 bags)",
+  ];
+  //compact car feature low rates and great fuel efficiency, with slightly more room than economy cars.
+  const availableTypes = ["Budget", "Standard", "Premium", "Luxury"];
+
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [vehicle, setVehicle] = useState("");
+  const [galleryImg, setGalleryImg] = useState(null);
+  const [duration, setDuration] = useState("");
+  const [description, setDescription] = useState("");
+  const [highlights, setHighlights] = useState("");
+  const [locations, setLocations] = useState("");
+  const [tourType, setTourType] = useState("");
+
+  const handleGalleryImg = (e) => {
+    setGalleryImg(e.target.files);
+  };
+
+  return (
+    <>
+      <c.Container>
+        <c.FormTitle>Request Custom Tour </c.FormTitle>
+        <c.Form>
+          <c.FormGroup>
+            <Label labelText={"Tour name"} />
+            <TextField
+              value={name}
+              setValue={setName}
+              placeholder={"Enter tour name here"}
+            />
+          </c.FormGroup>
+
+          <c.FormGroup>
+            <Label labelText={"Tour Category"} />
+            <DropDown
+              dropDownValues={availableCategory}
+              currentDropdownVal={category}
+              setCurrentDropdownVal={setCategory}
+            />
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Your Max Budget"} />
+            <TextField
+              value={price}
+              setValue={setPrice}
+              kplaceholder={"Enter tour price here"}
+            />
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Tour Images (if you have images upload here)"} />
+            <input
+              name="coverImg"
+              onChange={handleGalleryImg}
+              type="file"
+              placeholder={"Upload tour cover image here"}
+            />
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Preffered Vehicle"} />
+            <DropDown
+              dropDownValues={availableVehicle}
+              currentDropdownVal={vehicle}
+              setCurrentDropdownVal={setVehicle}
+            />
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Duration"} />
+            <TextField
+              value={duration}
+              setValue={setDuration}
+              type="text"
+              placeholder={"Enter tour duration here"}
+            />
+            k
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Tour Description"} />
+            <TextArea
+              value={description}
+              setValue={setDescription}
+              rows={10}
+              placeholder="Enter your tour request descrtiption here"
+            />
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Tour Highlights"} />
+            <TextArea
+              value={highlights}
+              setValue={setHighlights}
+              rows={10}
+              placeholder="Enter your tour request important information here"
+            />
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Tour Locations"} />
+            <TextArea
+              value={locations}
+              setValue={setLocations}
+              rows={10}
+              placeholder="Enter name and coordinates of the locations here
+            EX:
+            name_1 [latitude_1,longtitude_1]
+            name_2 [latitude_2,longtitude_2]
+            name_3 [latitude_3,longtitude_3]"
+            />
+          </c.FormGroup>
+          <c.FormGroup>
+            <Label labelText={"Select your tour package types"} />
+            <DropDown
+              dropDownValues={availableTypes}
+              currentDropdownVal={tourType}
+              setCurrentDropdownVal={setTourType}
+            />
+          </c.FormGroup>
+
+          <c.FormGroup>
+            <c.SubmitBtn type="submit">Submit Request</c.SubmitBtn>
+            <c.SubmitBtn color="#333" type="reset">
+              Clear
+            </c.SubmitBtn>
+          </c.FormGroup>
+        </c.Form>
+      </c.Container>
+    </>
+  );
+};
+
+export default ClientCustomTour;
