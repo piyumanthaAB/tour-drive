0a4126a84c939b3c71cd37a817abc9dbf9e39b0b
vmsoory <vmsoory@gmail.com>
2023-04-04 14:14:52 +0530

    created custom tour model

---
+++ server/models/customTourModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { stringify } from "ts-jest";
+
+const customTourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  category: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  vehicle: {
+    type: String,
+  },
+  duration: {
+    type: String,
+  },
+  description: {
+    type: String,
+    required: ["true", "description must be included."],
+  },
+  highlights: {
+    type: String,
+  },
+  location: {
+    type: [String],
+  },
+  tourType: {
+    type: String,
+    required: true,
+  },
+});
+
+const tour = mongoose.model("customTour", customTourSchema);
+export default customTour;
