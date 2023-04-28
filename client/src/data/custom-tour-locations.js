dbd879fd2cf9bc59e63929f839a4fd4477762ff7
cherin piyumantha <cherinpiyumantha@gmail.com>
2023-04-28 11:15:42 +0530

    custom tour data file

---
+++ client/src/data/custom-tour-locations.js
@@ -0,0 +1,34 @@
+const locations = [
+  {
+    city: 'galle',
+    availableLocations: [
+      'Unawatuna Beach',
+      'Galle Fort',
+      'Jungle Beach',
+      'Mihiripenna Beach',
+      'Japanese Peace Pagoda',
+    ],
+  },
+  {
+    city: 'matara',
+    availableLocations: [
+      'Parey Dewa Buddhist Temple',
+      'Weherahena Buddhist Temple',
+      'Dondra Head Lighthouse',
+      'Polhena Beach',
+      'Mirissa Beach',
+    ],
+  },
+  {
+    city: 'hambantota',
+    availableLocations: [
+      'Yala National Park',
+      'Bundala National Park',
+      'Tissamaharama ',
+      'Kataragama ',
+      'Sithulpawwa Rock Temple',
+    ],
+  },
+];
+
+export { locations as customTourLocations };
