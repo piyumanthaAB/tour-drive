016d831a17d8864085a5824308749d2f24799307
vmsoory <vmsoory@gmail.com>
2023-05-23 19:30:32 +0530

    home cover transistion editing

--- client/src/components/Home/HomeCoverElements.js
+++ client/src/components/Home/HomeCoverElements.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import tour from "../../images/tour_back.jpg";
 import renting from "../../images/renting.jpg";
+import { keyframes } from "styled-components";
 
 export const section = styled.div`
     min-width: 100%;
@@ -17,7 +18,7 @@ export const section = styled.div`
     // backdrop-filter: blur(12px);
     width: 100%;
     height: 800px;
-    transition: all 1.5s ease;
+    transition: all 1.2s ease;
 `;
 
 const hover = `
@@ -28,9 +29,16 @@ const hover = `
         }
     &:hover {
         flex-grow: 3;
+        transition-timing-function: cubic-bezier(0.55, 2.03, 0.17, 0.93);
         .test {
             display: block;
         }
+    }
+`;
+
+const animation = keyframes`
+    100%{
+      box-shadow: 0px 0px 100px 5px #000;
     }
 `;
 
@@ -48,10 +56,41 @@ export const btnTour = styled.a`
     text-decoration: none;
     border: 3px solid #fff;
     color: #fff;
+    font-size: 15px;
     font-weight: bold;
     text-transform: uppercase;
     text-align: center;
     box-shadow: 0px 10px 10px -5px rgba(0, 0, 0, 0.1);
+    transition: color 0.4s linear;
+    position: relative;
+  }
+  .btn:focus {
+    border-color: var(--secondary-color);
+    background: var(--secondary-color);
+    box-shadow: 0px 0px 2px 1px var(--secondary-color);
+    animation: ${animation} 0.2s forwards;
+  }
+  .btn: hover {
+    color: #000;
+  }
+  .btn: before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    z-index: -1;
+    transition: transform 0.5s;
+    transform-origin: 0 0;
+    transition-timing-function: cubic-bezier(0.5, 1.6, 0.4, 0.7);
+  }
+  .btn::before {
+    transform: scaleX(0);
+  }
+  .btn:hover::before {
+    transform: scaleX(1);
   }
 `;
 
@@ -61,7 +100,6 @@ export const panel = styled.div`
   position: relative;
   &:hover .overlay {
     opacity: 0.6;
-    // backdrop-filter: invert(80%);
     backdrop-filter: blur(12px);
   }
   &:nth-child(even) {
@@ -92,10 +130,41 @@ export const btnDrive = styled.a`
     text-decoration: none;
     border: 3px solid #fff;
     color: #fff;
+    font-size: 15px;
     font-weight: bold;
     text-transform: uppercase;
     text-align: center;
     box-shadow: 0px 10px 10px -5px rgba(0, 0, 0, 0.1);
+    transition: color 0.4s linear;
+    position: relative;
+  }
+  .btn:focus {
+    border-color: var(--main-color);
+    background: var(--main-color);
+    box-shadow: 0px 0px 2px 1px var(--main-color);
+    animation: ${animation} 0.2s forwards;
+  }
+  .btn: hover {
+    color: #000;
+  }
+  .btn: before {
+    content: "";
+    position: absolute;
+    left: 100%;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    z-index: -1;
+    transition: transform 0.5s;
+    transform-origin: 0 0;
+    transition-timing-function: cubic-bezier(0.5, 1.6, 0.4, 0.7);
+  }
+  .btn::before {
+    transform: scaleX(0);
+  }
+  .btn:hover::before {
+    transform: scaleX(-1);
   }
 `;
 
